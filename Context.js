bdc232695ce50c7f81bd2ad338ada599d76e6dea
Angel Guillen <angel1631@gmail.com>
2022-04-14 14:51:28 -0600

    act

--- Context.js
+++ Context.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Sequelize, Model, DataTypes } from 'sequelize';
+import mysql from "mysql2";
 
 const Context = React.createContext();
 
 function ContextProvider(props){
-    
-    let states = {};
+    let connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+    connection.connect(function(err) {
+        if (err) {
+          return console.error('error: ' + err.message);
+        }
+      
+        console.log('Connected to the MySQL server.');
+      });
+    let states = {orm:{models:[Coin]}};
+    //let states = {};
     return (
         <Context.Provider value={{
             states
