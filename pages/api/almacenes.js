9bc6ebe73a2abc3cda73372b13a6f1f64a68a05b
Angel Guillen <angel1631@gmail.com>
2022-04-28 18:18:10 -0600

    Update almacenes.js

--- pages/api/almacenes.js
+++ pages/api/almacenes.js
@@ -38,7 +38,7 @@ export default async function handler(req,res){
                 }else{
                     almacen.invertido += parseFloat(trade.precio)*parseFloat(trade.cantidad);
                 }
-                almacen.valor = almacen.invertido/almacen.acumulado;
+                almacen.valor = almacen.invertido/almacen.acumulado || 0;
             }
         });
         res.status(200).json({data: almacenes});
