2288df449d3d8a29b443a89407e5dce636c838fa
Angel Guillen <angel1631@gmail.com>
2022-04-28 22:17:19 -0600

    Update Almacen.js

--- components/Almacen.js
+++ components/Almacen.js
@@ -7,18 +7,15 @@ export default ({almacen})=>{
         <div className={`text-center py-2 px-4 shadow-xl rounded-lg `} >
             <div className='text-4xl py-4'>{name}</div>
             <div className=' px-3 py-2 '>
-                <div className='bg-stone-200 py-2 rounded-md'>
+                <div className={`${bg_historico}  py-2 rounded-md`}>
                     <div className='py-2 font-bold'> {symbol.toUpperCase()} {acumulado}</div>
                     <div className='grid grid-cols-2'>
                         <div>He invertido:</div><div> {format_currency({val:(invertido).toFixed(2)})}</div>
                         <div>Precio actual:</div><div> {format_currency({val:(price).toFixed(2)})}</div>    
-                    </div>    
-                </div>
-                <div className='text-left pl-2 font-bold mt-2'>Actual</div>
-                <div className={`${bg_historico} py-2 rounded-md grid grid-cols-2`}>
-                    <div>Valor</div><div>$. {format_currency({val:(valor).toFixed(2)})}</div>
-                    <div>Margen</div><div>{(margen*100).toFixed(2)}%</div>
-                    <div>Resultado</div><div> $. {(margen_USD).toFixed(2)}</div>
+                        <div>Valor</div><div>$. {format_currency({val:(valor).toFixed(2)})}</div>
+                        <div>Margen</div><div>{(margen*100).toFixed(2)}%</div>
+                        <div>Resultado</div><div> $. {(margen_USD).toFixed(2)}</div>
+                    </div>
                 </div>
                 <div className='text-left pl-2 font-bold mt-2'>Si liquido</div>
                 <div className={`${bg_futuro} py-2 rounded-sm grid grid-cols-2`}>
